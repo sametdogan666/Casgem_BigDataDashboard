4b0cb4b0be7a60784e7b5b57bf63126550732f92
Samet DOGAN <sametdogan23@protonmail.ch>
2023-08-03 17:57:50 +0300

    Add project files.

---
+++ Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿using BigDataDashboard.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Diagnostics;
+
+namespace BigDataDashboard.Controllers;
+
+public class CarController : Controller
+{
+    private readonly CarRepository _carRepository;
+    private readonly IMemoryCache _memoryCache;
+
+
+
+    public CarController(CarRepository carRepository, IMemoryCache memoryCache)
+    {
+        _carRepository = carRepository;
+        _memoryCache = memoryCache;
+    }
+
+    public async Task<IActionResult> Index(string searchString)
+    {
+        var benzinliAracSayisi = await _carRepository.GetBenzinliAracSayisiAsync();
+        ViewData["BenzinliAracSayisi"] = benzinliAracSayisi;
+
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Search(string searchString)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var allCars = await _carRepository.SearchCarsAsync(searchString);
+        stopwatch.Stop();
+
+        return Json(new { queryTime = stopwatch.Elapsed, data = allCars });
+    }
+}
---
+++ DAL/CarRepository.cs
@@ -0,0 +1,98 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BigDataDashboard.DAL;
+
+public class CarRepository
+{
+    private readonly string _connectionString;
+    private readonly IMemoryCache _memoryCache;
+
+
+    public CarRepository(IConfiguration configuration, IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    //public IEnumerable<Car> GetAllCars()
+    //{
+    //    using (IDbConnection connection = new SqlConnection(_connectionString))
+    //    {
+    //        connection.Open();
+    //        return connection.Query<Car>("SELECT * FROM PLATES");
+    //    }
+    //}
+
+    //public IEnumerable<Car> SearchCars(string keyword)
+    //{
+    //    using (IDbConnection connection = new SqlConnection(_connectionString))
+    //    {
+    //        connection.Open();
+    //        string sql = "SELECT * FROM PLATES WHERE Plate LIKE @keyword OR Brand LIKE @keyword OR Model LIKE @keyword";
+    //        return connection.Query<Car>(sql, new { keyword = $"%{keyword}%" });
+    //    }
+    //}
+
+    public async Task<IEnumerable<Car>> GetAllCarsAsync()
+    {
+        using (IDbConnection connection = new SqlConnection(_connectionString))
+        {
+            var cacheKey = "AllPlates";
+            if (_memoryCache.TryGetValue(cacheKey, out IEnumerable<Car> cars))
+            {
+                return cars;
+            }
+
+            connection.Open();
+            var query = "SELECT * FROM PLATES";
+            cars = await connection.QueryAsync<Car>(query);
+
+            // Verileri önbelleğe al
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) // Önbellekte 5 dakika tut
+            };
+
+            _memoryCache.Set(cacheKey, cars, cacheEntryOptions);
+
+            return cars;
+        }
+    }
+
+    public async Task<IEnumerable<Car>> SearchCarsAsync(string searchString)
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
+        var query =
+            $"SELECT * FROM PLATES WHERE BRAND LIKE @search";
+        return await connection.QueryAsync<Car>(query, new { search = $"%{searchString}%" });
+    }
+
+    public async Task<int> GetBenzinliAracSayisiAsync()
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString);
+        var cacheKey = "BenzinliAracSayisi";
+        if (_memoryCache.TryGetValue(cacheKey, out int count))
+        {
+            return count;
+        }
+
+        connection.Open();
+        count = await connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM PLATES WHERE FUEL = 'Benzin'");
+
+        // Veriyi önbelleğe al
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) // Önbellekte 5 dakika tut
+        };
+
+        _memoryCache.Set(cacheKey, count, cacheEntryOptions);
+
+        return count;
+    }
+
+}
